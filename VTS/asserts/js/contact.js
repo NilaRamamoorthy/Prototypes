0ea1f22e854715bf96bfed7ee668ccefe351a21c
NilaRamamoorthy <nilasibiraj@gmail.com>
2025-05-31 13:51:26 +0400

    Delete VTS directory

--- VTS/asserts/js/contact.js
+++
@@ -1,7 +0,0 @@
-// Contact Form Submission Handler
-        function handleContactForm(event) {
-            event.preventDefault(); // prevent actual form submission
-            const modal = new bootstrap.Modal(document.getElementById('contactModal'));
-            modal.show();
-            event.target.reset();
-        }
--- VTS/asserts/js/index.js
+++
@@ -1,6 +0,0 @@
-  //  JavaScript Alert on Submit
-        function handleForm(event) {
-            event.preventDefault();
-            alert('Message received!');
-            event.target.reset();
-        }
